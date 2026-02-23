5538b7d7d24d946b01a1b7a13a324ca3de94b57e
bpollak <brettcpollak@gmail.com>
2026-02-22 20:19:11 -0800

    Allow Next.js inline bootstrap scripts in CSP

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' blob:",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com https://cdn.ucsd.edu",
               "worker-src 'self' blob:",
